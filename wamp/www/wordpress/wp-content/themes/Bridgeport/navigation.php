8c468b68070347030ecf1db1bef5d67b8795a486
KaiserCreaux <creaux@gmail.com>
2015-08-13 19:48:51 -0400

    Moving Bridgeport to clean install of WAMP

---
+++ wamp/www/wordpress/wp-content/themes/Bridgeport/navigation.php
@@ -0,0 +1,40 @@
+<!--start navigationmenu-->
+<header id="sectionNavigation">
+	<div class="container clearfix">
+    <div class="row hide-on-med-and-down"> <!-- Desktop bio information -->
+		<div class="col s12"><h5 class="center-align">Bridgeport Presbyterian &nbsp;|&nbsp; 616 DEKALB PIKE, BRIDGEPORT PA 13405</h5></div>
+	</div>
+ 	<!--start container-->
+	<div class="row">
+		<!--left navigation-->
+    	<div class="col s12">
+            <nav class="centerNav">
+				<div class="nav-wrapper">
+				<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons white">menu</i></a>
+        		<ul class="right hide-on-med-and-down">
+                    <li class=" red accent-4"><a href="#sectionMenu" class="flow-text white">LEARN ABOUT US</a></li> 
+                    <li><a href="#sectionCatering" class="flow-text white">SCHEDULE AND EVENTS</a></li> 
+                	<li><a href="#sectionContact" class="flow-text white">OUR COMMUNITY</a></li> 
+                    <li><a href="#sectionMap" class="flow-text white">CONTACT US</a></li> 
+                </ul>
+				<ul class="side-nav" id="mobile-demo">
+                    <li><a href="#sectionMenu" class="flow-text white">LEARN ABOUT US</a></li> 
+                    <li><a href="#sectionCatering" class="flow-text white">SCHEDULE AND EVENTS</a></li> 
+                	<li><a href="#sectionContact" class="flow-text white">OUR COMMUNITY</a></li> 
+                    <li><a href="#sectionMap" class="flow-text white">CONTACT US</a></li> 
+				</ul>
+				</div>
+            </nav>
+        </div>
+        <!--end right navigation-->
+    
+	</div>
+	<div class="row hide-on-large-only"> <!-- Mobile and Tablet bio information -->
+		<div class="col s12"><h5 class="center-align">Bridgeport Presbyterian &nbsp;|&nbsp; 616 DEKALB PIKE, BRIDGEPORT PA 13405</h5></div>
+	</div>
+	
+	</div>
+    <!--end container-->   
+    </div>
+</header>
+<!--end navigationmenu-->
